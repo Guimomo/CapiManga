3608d7b2c9988d31d045225e69199a942b481dc3
Guillermo Garrido <gnilleuni@gmail.com>
2024-11-19 01:51:19 -0500

    UpgradeVersion

--- JS/login.js
+++ JS/login.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const loginButton = document.querySelector(".login-button");
-    const menuLogin = document.querySelector(".menu-login");
+    // const loginButton = document.querySelector(".login-button");
+    // const menuLogin = document.querySelector(".menu-login");
 
-    loginButton.addEventListener("click", () => {
-        menuLogin.classList.toggle("menu-visible"); // Alterna la clase "menu-visible"
-    });
+    // loginButton.addEventListener("click", () => {
+    //     menuLogin.classList.toggle("menu-visible"); // Alterna la clase "menu-visible"
+    // });
     
+    // Obtener el checkbox
+    const loginCheckbox = document.querySelector(".login-checkbox");
+    
+    // Asegurar que esté desmarcado al cargar la página
+    loginCheckbox.checked = false;
 });
