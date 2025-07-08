efc990edac7c434aaaa25d6a7de439df570dfcfc
Guillermo Garrido <gnilleuni@gmail.com>
2025-07-07 19:18:53 -0500

    > Cambios antes de presentacion

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,4 @@
+import { Autenticado } from "./src/helpers/auth.js";
 import Header from "./src/js/dom/header.js";
 import { router } from "./src/router/router.js";
 
@@ -5,6 +6,14 @@ import { router } from "./src/router/router.js";
 const app = document.querySelector('#app');
 
 Header();
+
+const subir_crear = document.querySelector('.subir_crear');
+
+if (!Autenticado()) {
+
+    // subir_crear.style.display = 'none';
+    subir_crear.remove();
+}
 
 
 window.addEventListener('hashchange', () => {
